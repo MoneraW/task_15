5cc2aa0e7d68ae2fafb66de08656fd51f46c2f98
moneraw <wogayanm@gmail.com>
2018-08-05 15:49:11 +0300

    task 15

--- api/serializers.py
+++ api/serializers.py
@@ -4,8 +4,19 @@ from restaurants.models import Restaurant
 class RestaurantListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Restaurant
-        fields = [
+        fields = [ 'id',
         	'name',
         	'opening_time',
         	'closing_time',
         	]
+
+class RestaurantDetailSerilizer(serializers.ModelSerializer):
+	class Meta:
+		model = Restaurant
+		fields = ['id', 'owner', 'name', 'description', 'opening_time', 'closing_time',]
+
+
+class RestaurantCreateSerilizer(serializers.ModelSerializer):
+	class Meta:
+		model = Restaurant
+		fields = ['name', 'description', 'opening_time', 'closing_time',]
